dd686fcb36bc987143ede1d51f9d1b0ea78eef40
Thubelihle <barbranoxie@gmail.com>
2023-02-22 15:36:48 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,8 @@ void print_alphabet_x10(void)
 {
 char i;
 int x;
-<<<<<<< HEAD
 for(x=0;x < 10 ;x++)
-=======
 for (x = 0; x < 10 ; x++)
->>>>>>> 80e12e64344ced165a1cff86ef70abbd3c60b7b7
 {
 for (i = 'a'; i <= 'z'; i++)
 {
