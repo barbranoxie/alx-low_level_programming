86a43ae0fbfa3affb00b8e0caa7e380d066bb75c
Thubelihle <barbranoxie@gmail.com>
2023-02-27 17:18:43 +0200

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_rev - a function that prints a string, in reverse,
@@ -8,13 +8,13 @@
  */
 void print_rev(char *s)
 {
-	int len = 0;
+int len = 0;
 
-	while (s[len] != '\0')
+while (s[len] != '\0')
 		len++;
 
-	while (len)
-		_putchar(s[--len]);
+while (len)
+_putchar(s[--len]);
 
-	_putchar('\n');
+_putchar('\n');
 }
