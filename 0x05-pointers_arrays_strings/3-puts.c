2da1b4c0ca39f46071e71f2e2252a63783130d5c
Thubelihle <barbranoxie@gmail.com>
2023-02-27 17:16:37 +0200

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _puts - Write a function that prints a string, followed
@@ -8,12 +8,12 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
+int i = 0;
 
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
