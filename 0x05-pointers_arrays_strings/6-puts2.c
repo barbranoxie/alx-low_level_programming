7385d3de61a852dfe173eaaa51c562cf10330467
Thubelihle <barbranoxie@gmail.com>
2023-02-27 17:24:12 +0200

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * puts2 - a function that prints every other character of a string,
@@ -8,15 +8,15 @@
  */
 void puts2(char *str)
 {
-	int len = 0, i = 0;
+int len = 0, i = 0;
 
-	while (str[len] != '\0')
-		len++;
+while (str[len] != '\0')
+len++;
 
-	len -= 1;
+len -= 1;
 
-	for (; i <= len; i += 2)
-		_putchar(str[i]);
+for (; i <= len; i += 2)
+_putchar(str[i]);
 
-	_putchar('\n');
+_putchar('\n');
 }
